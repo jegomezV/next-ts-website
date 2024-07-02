30fcf2ab957e0a796606af9cc80887bcfa80f5ad
jegomezV <jegomezvalencia@gmail.com>
2024-07-02 16:42:53 -0500

    Reponsive changes

--- app/components/homeComponents/Header.tsx
+++ app/components/homeComponents/Header.tsx
@@ -26,55 +26,64 @@ export const HeaderM: React.FC = (): JSX.Element => {
       <Nav />
 
       {/* Main header section with title */}
-      <div className='h-[100vh] border-b-[1px] border-white/30'>
+      <div className="h-[100vh] border-b-[1px] border-white/30">
         <div className="w-screen h-full flex items-center justify-center">
-          <div className="animate-glow md:block animate-fade-left bg-gradient-to-r from-zinc-300/0 via-zinc-300/0 to-zinc-300/0" />
-          <h1 className="z-10 text-2xl text-transparent duration-1000 bg-white cursor-default text-edge-outline animate-title font-display sm:text-5xl md:text-6xl xl:text-8xl whitespace-nowrap bg-clip-text">
-            What about beauty?
+          <div className="
+                        animate-glow md:block animate-fade-left
+                        bg-gradient-to-r from-zinc-300/0 via-zinc-300/0 to-zinc-300/0" />
+          <h1 className="
+                        z-10 text-2xl text-transparent duration-1000
+                        bg-white cursor-default text-edge-outline
+                        animate-title font-display sm:text-5xl md:text-6xl xl:text-8xl
+                        whitespace-nowrap bg-clip-text"> What about beauty?
           </h1>
         </div>
       </div>
 
+
       {/* Absolute positioned images with hover effects */}
       <div className='absolute top-0 w-screen h-screen'>
         <Parallax scale={[1, 1]} translateX={[50, 0]} className="
-          absolute left-40 bottom-10
+          absolute left-[35%] bottom-36
           border-[1px] border-white/10 rounded
           h-60 w-44
-          sm:left-10
+          sm:left-[49%] sm:bottom-20 sm:h-52 sm:w-44
+          md:left-[56%] md:h-72 md:w-56 md:bottom-28
           lg:left-28
-          xl:h-72 xl:w-64 xl:left-40 xl:bottom-7
+          xl:h-96 xl:w-80 xl:left-[50%] xl:bottom-20
           2xl:left-[4rem] 2xl:bottom-[8rem] 2xl:h-96 2xl:w-72
           hover:translate-x-5 hover:duration-300
           duration-300">
           <Image
             fill
-
             src={artAlbine.src}
             alt="Picture of the author"
           />
         </Parallax>
         <Parallax scale={[1, 1]} translateX={[-50, 0]} className="
-          absolute right-16
+          absolute left-[65%] top-6
           border-[1px] border-white/10 rounded
           h-52 w-36
-          lg:top-16 lg:right-4
+          sm:left-[67%] sm:top-3
+          md:left-[78%] md:h-60 md:w-44
+          lg:top-16 lg:right-40
+          xl:left-[84%] xl:h-72 xl:w-52 xl:top-16
           2xl:top-12 2xl:h-[30rem] 2xl:w-72
           hover:-translate-x-1 hover:translate-y-1 hover:duration-300
           duration-300">
           <Image
             fill
-
             src={secondPhoto.src}
             alt="Picture of the author"
           />
         </Parallax>
         <Parallax scale={[1, 1]} translateX={[50, 0]} className="
-          absolute top-10
+          absolute top-16 -left-[4%]
           border-[1px] border-white/10 rounded
-          h-72 w-32
-          sm:top-20 sm:h-52 sm:w-40
-          xl:h-64 xl:w-[250px]
+          h-52 w-36
+          sm:top-10 sm:h-56 sm:w-44
+          md:-left-[4%] md:h-80 md:w-60
+          xl:h-96 xl:w-[18rem]
           2xl:h-96 2xl:w-72 2xl:top-20
           hover:translate-y-3 hover:duration-300
           duration-300">
@@ -87,12 +96,13 @@ export const HeaderM: React.FC = (): JSX.Element => {
 
         </Parallax>
         <Parallax scale={[1, 1]} translateX={[50, 0]} className="
-          absolute right-80 bottom-0
+          absolute right-72 bottom-24
           border-[1px] border-white/10 rounded
-          h-60 w-44
-          sm:right-12
+          h-60 w-40
+          sm:right-[80%] sm:bottom-30 sm:h-52 sm:w-36
+          md:h-60 md:w-44 md:right-[80%] md:bottom-28
           lg:right-72
-          xl:h-72 xl:w-52 xl:right-80
+          xl:h-72 xl:w-52 xl:left-0 xl:bottom-20
           2xl:right-[40rem] 2xl:bottom-[8rem] 2xl:h-[500px] 2xl:w-[400px] 
           hover:translate-x-5 hover:-translate-y-2 hover:duration-300
           duration-300
@@ -106,18 +116,18 @@ export const HeaderM: React.FC = (): JSX.Element => {
         </Parallax>
 
         <div className="
-          absolute -bottom-28 right-1/4
+          absolute bottom-[25%] left-[50%]
           transform -translate-x-1/2 -translate-y-1/2
           border-[1px] border-white/10 rounded
-          h-96 w-60
-          sm:h-72 sm:w-52 sm:bottom-10 sm:right-[70px]
+          h-64 w-52
+          sm:h-72 sm:w-52 sm:bottom-20 sm:left-[47%]
+          md:left-[46%] md:bottom-10 md:h-[500px] md:w-[350px]
           lg:h-[400px] lg:w-[300px] lg:right-72 lg:-bottom-20
-          xl:h-[410px] xl:w-[350px] xl:-bottom-22 xl:right-[425px]
+          xl:h-[40rem] xl:w-[29rem] xl:-bottom-[20%] xl:right-[425px]
           2xl:h-[800px] 2xl:w-[600px] 2xl:right-[900px] 2xl:top-[35rem]">
 
           <Image
             fill
-
             src={girlPick.src}
             alt="Picture of the author"
           />
@@ -127,17 +137,17 @@ export const HeaderM: React.FC = (): JSX.Element => {
         <Parallax scale={[1, 1]} translateX={[-30, 0]} className="
           absolute right-32 top-36
           border-[1px] border-white/10 rounded
-          h-36 w-24
+          h-0 w-0
           sm:w-0 sm:h-0
-          md:h-36 md:w-24
+          sm:right-56 sm:top-5
+          md:h-44 md:w-36 md:right-36 md:top-48
           lg:top-64
-          xl:h-44 xl:w-32 xl:top-64 xl:right-24
+          xl:h-64 xl:w-48 xl:top-64 xl:right-48
           2xl:right-[20rem] 2xl:top-32 2xl:h-96 2xl:w-72
           hover:-translate-x-5 hover:-translate-y-2 hover:duration-300
           duration-300">
           <Image
             fill
-
             src={buttersHead.src}
             alt="Picture of the author"
           />
@@ -145,11 +155,11 @@ export const HeaderM: React.FC = (): JSX.Element => {
         <Parallax scale={[1, 1]} translateX={[-50, 0]} className="
           absolute right-60 top-40
           border-[1px] border-white/10 rounded
-          h-60 w-36
+          h-0 w-0
           sm:w-0 sm:h-0
-          md:h-40 md:w-28 md:left-40 md:top-58
+          md:h-40 md:w-32 md:left-20 md:top-80
           lg:left-[40rem] lg:top-32
-          xl:left-[40rem] xl:h-44 xl:w-36 xl:top-96
+          xl:left-[22rem] xl:h-52 xl:w-44 xl:top-[55%]
           2xl:left-[30rem] 2xl:top-96 2xl:h-96 2xl:w-72
           hover:translate-x-3 hover:translate-y-3 hover:duration-300
           duration-300">
