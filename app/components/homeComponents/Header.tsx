5b1400ffd3f112550555f026e58f2861a837ab3f
jegomezV <jegomezvalencia@gmail.com>
2024-07-03 16:37:09 -0500

    2xl adds fixes

--- app/components/homeComponents/Header.tsx
+++ app/components/homeComponents/Header.tsx
@@ -13,7 +13,6 @@ import girlPick from '../../../public/homeImgs/homeBg/girlPick.jpg';
 import buttersHead from '../../../public/homeImgs/homeBg/buttersHead.jpg';
 import shadowGirl from '../../../public/homeImgs/homeBg/shadowGirl.jpg';
 
-
 // Importing Parallax component from react-scroll-parallax for parallax effect
 import { Parallax } from 'react-scroll-parallax';
 
@@ -28,13 +27,13 @@ export const HeaderM: React.FC = (): JSX.Element => {
       <div className="h-[100vh] border-b-[1px] border-white/30">
         <div className="w-screen h-full flex items-center justify-center">
           <div className="
-                        animate-glow md:block animate-fade-left
-                        bg-gradient-to-r from-zinc-300/0 via-zinc-300/0 to-zinc-300/0" />
+            animate-glow md:block animate-fade-left
+            bg-gradient-to-r from-zinc-300/0 via-zinc-300/0 to-zinc-300/0" />
           <h1 className="
-                        z-10 text-2xl text-transparent duration-1000
-                        bg-white cursor-default text-edge-outline
-                        animate-title font-display sm:text-5xl md:text-6xl xl:text-8xl
-                        whitespace-nowrap bg-clip-text"> What about beauty?
+            z-10 text-2xl text-transparent duration-1000
+            bg-white cursor-default text-edge-outline
+            animate-title font-display sm:text-5xl md:text-6xl xl:text-8xl
+            whitespace-nowrap bg-clip-text"> What about beauty?
           </h1>
         </div>
       </div>
@@ -47,8 +46,8 @@ export const HeaderM: React.FC = (): JSX.Element => {
           h-60 w-44
           sm:left-[49%] sm:bottom-20 sm:h-52 sm:w-44
           md:left-[56%] md:h-72 md:w-56 md:bottom-28
-          xl:h-96 xl:w-80 xl:left-[50%] xl:bottom-12
-          2xl:h-[30rem] 2xl:w-[25rem] 2xl:left-[50%] 2xl:bottom-[1rem]
+          xl:h-80 xl:w-80 xl:left-[50%] xl:bottom-12
+          2xl:h-[25rem] 2xl:w-[20rem] 2xl:left-[50%] 2xl:bottom-[1rem]
           hover:translate-x-5 hover:duration-300
           duration-300">
           <Image
@@ -85,11 +84,9 @@ export const HeaderM: React.FC = (): JSX.Element => {
           duration-300">
           <Image
             fill
-
             src={cameraMan.src}
             alt="Picture of the author"
           />
-
         </Parallax>
         <Parallax scale={[1, 1]} translateX={[50, 0]} className="
           absolute right-72 bottom-24 z-20
@@ -97,14 +94,12 @@ export const HeaderM: React.FC = (): JSX.Element => {
           h-60 w-40
           sm:right-[80%] sm:bottom-30 sm:h-52 sm:w-36
           md:h-60 md:w-44 md:right-[80%] md:bottom-28
-          xl:h-72 xl:w-60 xl:left-0 xl:bottom-0
-          2xl:h-96 2xl:w-72 2xl:left-[22rem] 2xl:bottom-[2rem]
+          xl:h-52 xl:w-46 xl:left-0 xl:bottom-0
+          2xl:h-80 2xl:w-64 2xl:left-0 2xl:bottom-[0rem]
           hover:translate-x-5 hover:-translate-y-2 hover:duration-300
-          duration-300
-        ">
+          duration-300">
           <Image
             fill
-
             src={albineMan.src}
             alt="Picture of the author"
           />
@@ -117,16 +112,13 @@ export const HeaderM: React.FC = (): JSX.Element => {
           h-64 w-52
           sm:h-72 sm:w-52 sm:bottom-20 sm:left-[47%]
           md:left-[46%] md:bottom-10 md:h-[500px] md:w-[350px]
-          xl:h-[35rem] xl:w-[29rem] xl:-bottom-[20%] xl:right-[425px]
-          2xl:h-[40rem] 2xl:w-[32rem] 2xl:right-[450px] 2xl:-bottom-[10%]
-          ">
-
+          xl:h-[30rem] xl:w-[25rem] xl:-bottom-[20%] xl:right-[425px]
+          2xl:h-[40rem] 2xl:w-[32rem] 2xl:right-[450px] 2xl:-bottom-[24%]">
           <Image
             fill
             src={girlPick.src}
             alt="Picture of the author"
           />
-
         </div>
 
         <Parallax scale={[1, 1]} translateX={[-30, 0]} className="
@@ -134,10 +126,9 @@ export const HeaderM: React.FC = (): JSX.Element => {
           border-[1px] border-white/10 rounded
           h-0 w-0
           sm:w-0 sm:h-0
-          sm:right-56 sm:top-5
           md:h-44 md:w-36 md:right-36 md:top-48
-          xl:h-64 xl:w-48 xl:top-64 xl:right-48
-          2xl:h-80 2xl:w-60 2xl:right-[15rem] 2xl:top-[21rem]
+          xl:h-[13rem] xl:w-[10rem] xl:top-36 xl:right-52
+          2xl:h-64 2xl:w-52 2xl:right-[15rem] 2xl:top-[9rem]
           hover:-translate-x-5 hover:-translate-y-2 hover:duration-300
           duration-300">
           <Image
@@ -153,12 +144,11 @@ export const HeaderM: React.FC = (): JSX.Element => {
           sm:w-0 sm:h-0
           md:h-40 md:w-32 md:left-20 md:top-80
           xl:left-[23rem] xl:h-52 xl:w-48 xl:top-[55%]
-          2xl:h-80 2xl:w-60 2xl:left-[20rem] 2xl:top-[36%]
+          2xl:h-72 2xl:w-52 2xl:left-[25rem] 2xl:top-[55%]
           hover:translate-x-3 hover:translate-y-3 hover:duration-300
           duration-300">
           <Image
             fill
-
             src={shadowGirl.src}
             alt="Picture of the author"
           />
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,13 @@ module.exports = {
 	],
 
 	theme: {
+		screens: {
+			'sm': '640px',
+			'md': '768px',
+			'lg': '1024px',
+			'xl': '1280px',
+			'2xl': '1536px',
+		  },
 		extend: {
 			spacing: {
         '18rem': '18rem',
