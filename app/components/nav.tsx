1123672e72584123635c7f7212bf19836259bd74
jegomezV <jegomezvalencia@gmail.com>
2024-07-09 20:22:14 -0500

    Nav fixes finish 7/9/24

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -12,47 +12,88 @@ export const Nav: React.FC = () => {
   };
 
   useEffect(() => {
-    if (isOpen) {
-      gsap.to(menuRef.current, {
-        duration: 1,
-        height: '100vh',
-        ease: "none",
-        onStart: () => {
-          menuRef.current?.classList.remove('max-sm:hidden');
-        },
-      });
+    const handleResize = () => {
+      if (window.innerWidth <= 640) {
+        if (isOpen) {
+          gsap.fromTo(
+            menuRef.current,
+            { height: '0vh', opacity: 0 },
+            {
+              duration: 0.5,
+              height: '100vh',
+              opacity: 1,
+              ease: 'none',
+              onStart: () => {
+                menuRef.current?.classList.remove('max-sm:hidden');
+              },
+            }
+          );
+        } else {
+          gsap.fromTo(
+            menuRef.current,
+            { height: '100vh', opacity: 1 },
+            {
+              duration: 0.6,
+              height: '0vh',
+              opacity: 0.7,
+              ease: 'none',
+              onComplete: () => {
+                menuRef.current?.classList.add('max-sm:hidden');
+              },
+            }
+          );
+        }
+      } else {
+        if (isOpen) {
+          gsap.to(menuRef.current, {
+            duration: 0.5,
+            height: 'auto',
+            opacity: 1,
+            ease: 'none',
+            onStart: () => {
+              menuRef.current?.classList.remove('max-sm:hidden');
+            },
+          });
+        } else {
+          gsap.to(menuRef.current, {
+            duration: 0.7,
+            height: 'auto',
+            opacity: 1,
+            ease: 'power3.inOut',
+            onComplete: () => {
+              menuRef.current?.classList.add('max-sm:hidden');
+            },
+          });
+        }
+      }
+    };
 
-    } else {
-      gsap.to(menuRef.current, {
-        duration: 0.6,
-        height: '-100vh',
-        ease: "none",
-        onComplete: () => {
-          menuRef.current?.classList.add('max-sm:hidden');
-        },
-      });
-    }
+    handleResize();
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
   }, [isOpen]);
 
   return (
     <div className='w-screen mx-auto'>
       <button
         className="
-                menu-toggle-button
-                fixed 
-                right-[6px] 
-                top-[4.9rem] 
-                z-[100]
-                flex 
-                items-center 
-                justify-center 
-                rounded-full 
-                border-white 
-                backdrop-blur-3xl 
-                menu-toggle-button 
-                max-sm:h-10 max-sm:w-10 max-sm:p-[4px] max-sm:border-[1px]"
-        onClick={toggleMenu}>
-
+          menu-toggle-button
+          fixed 
+          right-[6px] 
+          top-[4.9rem] 
+          z-[100]
+          flex 
+          items-center 
+          justify-center 
+          rounded-full 
+          border-white 
+          backdrop-blur-3xl 
+          max-sm:h-10 max-sm:w-10 max-sm:p-[4px] max-sm:border-[1px]"
+        onClick={toggleMenu}
+      >
         <svg
           xmlns="http://www.w3.org/2000/svg"
           fill="none"
@@ -69,21 +110,21 @@ export const Nav: React.FC = () => {
         </svg>
       </button>
 
-      <nav 
+      <nav
         ref={menuRef}
-        className='
-          fixed max-sm:hidden max-sm:h-0 max-sm:border-y-[1px] max-sm:border-white top-8 left-0 right-0 mx-auto mt-0 py-10 z-50
+        className="
+          fixed max-sm:hidden max-sm:h-0 max-sm:border-y-[1px] max-sm:border-white max-sm:top-0 top-8 left-0 right-0 z-50
           flex max-sm:flex-col items-center justify-center sm:flex-row space-x-40
-          sm:w-[100%] sm:mx-auto sm:py-[14px]
+          sm:w-full sm:py-[14px]
           hover:duration-500 duration-500 hover:shadow-white/20 shadow-inner shadow-white/10
           border-y-[1px] wrapper border-white/20 backdrop-blur-3xl
-          overflow-hidden'>
+          overflow-hidden">
 
         <ul className="
-          flex flex-col max-sm:space-y-8 max-sm:justify-center max-sm:items-center sm:flex-row space-x-2 md:text-sm
-          md:space-x-20 lg:text-base xl:text-base lg:space-x-40
-          hover:text-white text-xl">
-
+          flex flex-col max-sm:space-y-8 max-sm:justify-center max-sm:items-center max-sm:text-2xl sm:flex-row space-x-2 md:text-lg
+          md:space-x-20 lg:text-xl xl:text-xl lg:space-x-40
+          hover:text-white 2xl:text-xl"
+        >
           <li className="sm:inline-block hidden">
             <Link
               href="/"
@@ -91,7 +132,9 @@ export const Nav: React.FC = () => {
                 relative max-sm:text-white bg-[linear-gradient(#00000000,#00000000),linear-gradient(#ffffff,#ffffff)]
                 bg-[length:100%_2px,0_2px] bg-[position:100%_100%,0_100%] bg-no-repeat
                 text-gray-300 transition-[background-size,color] duration-300
-                hover:bg-[0_2px,100%_2px] hover:text-[#ffffff]"> Home
+                hover:bg-[0_2px,100%_2px] hover:text-[#ffffff] "
+            >
+              Home
             </Link>
           </li>
           <li>
@@ -101,7 +144,9 @@ export const Nav: React.FC = () => {
                 relative max-sm:text-white bg-[linear-gradient(#00000000,#00000000),linear-gradient(#ffffff,#ffffff)]
                 bg-[length:100%_2px,0_2px] bg-[position:100%_100%,0_100%] bg-no-repeat
                 text-gray-300 transition-[background-size,color] duration-300
-                hover:bg-[0_2px,100%_2px] hover:text-[#ffffff]"> About
+                hover:bg-[0_2px,100%_2px] hover:text-[#ffffff]"
+            >
+              About
             </Link>
           </li>
           <li>
@@ -111,7 +156,9 @@ export const Nav: React.FC = () => {
                 relative max-sm:text-white bg-[linear-gradient(#00000000,#00000000),linear-gradient(#ffffff,#ffffff)]
                 bg-[length:100%_2px,0_2px] bg-[position:100%_100%,0_100%] bg-no-repeat
                 text-gray-300 transition-[background-size,color] duration-300
-                hover:bg-[0_2px,100%_2px] hover:text-[#ffffff]"> Credits
+                hover:bg-[0_2px,100%_2px] hover:text-[#ffffff]"
+            >
+              Credits
             </Link>
           </li>
           <li>
@@ -121,13 +168,14 @@ export const Nav: React.FC = () => {
                 relative max-sm:text-white bg-[linear-gradient(#00000000,#00000000),linear-gradient(#ffffff,#ffffff)]
                 bg-[length:100%_2px,0_2px] bg-[position:100%_100%,0_100%] bg-no-repeat
                 text-gray-300 transition-[background-size,color] duration-300
-                hover:bg-[0_2px,100%_2px] hover:text-[#ffffff]"> Models
+                hover:bg-[0_2px,100%_2px] hover:text-[#ffffff]"
+            >
+              Models
             </Link>
           </li>
         </ul>
         <TranslateButton />
       </nav>
-
     </div>
   );
 };
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import "../global.css";
+import "./styles/fonts.css";
 import { Inter } from "@next/font/google";
 import LocalFont from "@next/font/local";
 import { Metadata } from "next";
