bc3f09f6d942f83ca4d569d5739607aa9968a89d
jegomezV <jegomezvalencia@gmail.com>
2024-08-30 17:01:21 -0500

    all sections photos added fix

--- app/components/actorsPage/modelDiego/sections/GalleryDiego.tsx
+++ app/components/actorsPage/modelDiego/sections/GalleryDiego.tsx
@@ -18,8 +18,8 @@ const GalleryDiego: React.FC = (): JSX.Element => {
   return (
     <section className='relative w-full py-16 bg-slate-200'>
       <div className='max-w-7xl mx-auto'>
-        <h1 
-          data-scroll 
+        <h1
+          data-scroll
           data-scroll-speed="-0.2"
           className="text-2xl p-2 text-center text-transparent duration-1000 bg-white drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)] cursor-default font-display sm:text-3xl md:text-4xl xl:text-6xl bg-clip-text"
         >
@@ -28,31 +28,33 @@ const GalleryDiego: React.FC = (): JSX.Element => {
 
         <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-1 lg:grid-cols-3 gap-4 mt-8'>
           {photos.map((photo, index) => (
-            <div key={index} className='relative w-full h-[40vw]'>
-              <Image 
-                src={photo} 
-                alt={`Diego Biography ${index + 1}`} 
-                fill 
-                objectFit="cover" 
-                placeholder="blur" 
-                className='drop-shadow-[0_1.5px_20px_rgba(0,0,0,1)] hover:scale-105 transition-transform duration-200 cursor-pointer'
-                onClick={() => setSelectedPhoto(photo.src)}
-              />
-            </div>
+            photo && (
+              <div key={index} className='relative w-full h-[40vw]'>
+                <Image
+                  src={photo}
+                  alt={`Diego Biography ${index + 1}`}
+                  fill
+                  objectFit="cover"
+                  placeholder="blur"
+                  className='drop-shadow-[0_1.5px_20px_rgba(0,0,0,1)] hover:scale-105 transition-transform duration-200 cursor-pointer'
+                  onClick={() => setSelectedPhoto(photo.src)}
+                />
+              </div>
+            )
           ))}
         </div>
 
         {selectedPhoto && createPortal(
           <div className="fixed inset-0 bg-black bg-opacity-75 flex items-center justify-center z-50" onClick={() => setSelectedPhoto(null)}>
             <div className="relative w-11/12 max-w-4xl h-auto">
-              <Image 
-                src={selectedPhoto} 
-                alt="Selected Image" 
-                layout="responsive" 
-                width={100} 
-                height={75} 
-                objectFit="contain" 
-                className='cursor-pointer' 
+              <Image
+                src={selectedPhoto}
+                alt="Selected Image"
+                layout="responsive"
+                width={100}
+                height={75}
+                objectFit="contain"
+                className='cursor-pointer'
               />
               <button className="absolute top-4 right-4 text-white text-2xl" onClick={() => setSelectedPhoto(null)}>✕</button>
             </div>
